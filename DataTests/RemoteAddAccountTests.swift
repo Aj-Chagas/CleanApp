81b873ed2e120d44a98c642caf3c8ddeb184c409
andersonchagas <anderson.1997reis@gmail.com>
2020-09-13 11:29:26 -0300

    refactor RemoteAddAccount Tests

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -32,19 +32,28 @@ class RemoteAddAccountTests: XCTestCase {
 
     func test_add_should_call_httpClient_with_correct_url() {
         let url = URL(string: "http://any-url.com")!
-        let httpClientSpy = HttpClientSpy()
-        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
-        sut.add(addAccountModel: AddAccountModel(name: "any_name", email: "any_email", password: "any_password", passwordConfirmation: "any_password"))
+        let (sut, httpClientSpy) = makeSut(url: url)
+        sut.add(addAccountModel: makeAddAccountModel())
         XCTAssertEqual(httpClientSpy.url, url)
     }
     
     func test_add_should_call_httpClient_with_correct_data() {
-        let url = URL(string: "http://any-url.com")!
-        let httpClientSpy = HttpClientSpy()
-        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
-        let addAccountModel = AddAccountModel(name: "any_name", email: "any_email", password: "any_password", passwordConfirmation: "any_password")
+        let (sut, httpClientSpy) = makeSut()
+        let addAccountModel = makeAddAccountModel()
         sut.add(addAccountModel: addAccountModel)
         XCTAssertEqual(httpClientSpy.data, addAccountModel.toData())
+    }
+}
+
+extension RemoteAddAccountTests {
+    func makeSut(url: URL = URL(string: "http://any-url.com")!) -> (sut: RemoteAddAccount, httpClientSpy: HttpClientSpy ){
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
+        return (sut, httpClientSpy)
+    }
+    
+    func makeAddAccountModel() -> AddAccountModel {
+        return AddAccountModel(name: "any_name", email: "any_email", password: "any_password", passwordConfirmation: "any_password")
     }
     
     class HttpClientSpy: HttpPostClient {
@@ -56,5 +65,4 @@ class RemoteAddAccountTests: XCTestCase {
             self.data = data
         }
     }
-
 }
