d2370a0f7eae90d0655e3314bcc05c6af658b299
Anderson Chagas <anderson.1997reis@gmail.com>
2021-02-06 16:49:03 -0300

    create infra layer

---
+++ InfraTests/AlamofireAdapterTest.swift
@@ -0,0 +1,18 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Anderson Chagas on 06/02/21.
+//  Copyright © 2021 Anderson Chagas. All rights reserved.
+//
+
+import XCTest
+@testable import Infra
+
+class AlamofireAdapterTest: XCTestCase {
+
+    func test() {
+        
+    }
+
+}
