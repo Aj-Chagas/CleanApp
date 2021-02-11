f5bfdb6df4d475a52ee0e2881764eddbd2c34758
Anderson Chagas <anderson.1997reis@gmail.com>
2021-02-11 19:49:39 -0300

    ensure alamofireAdapter calls alamofire with correct data

--- DataTests/TestFactories.swift
+++ DataTests/TestFactories.swift
@@ -12,6 +12,10 @@ func makeInvalidData() -> Data {
     Data("invalid_data".utf8)
 }
 
+func makeValidData() -> Data {
+    Data("{\"name\": \"Anderson\"}".utf8)
+}
+
 func makeUrl() -> URL {
     URL(string: "http://any-url.com")!
 }
