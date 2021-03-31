40e08e7becb70351efa50a0b63106522b65a2ff7
Anderson Chagas <anderson.1997reis@gmail.com>
2021-03-31 17:08:53 -0300

    move class emailValidatorSpy to own file

---
+++ PresentationTests/Validators/EmailValidatorSpy.swift
@@ -0,0 +1,24 @@
+//
+//  EmailValidatorSpy.swift
+//  PresentationTests
+//
+//  Created by Anderson Chagas on 31/03/21.
+//  Copyright © 2021 Anderson Chagas. All rights reserved.
+//
+
+import Foundation
+import Presentation
+
+class EmailValidatorSpy: EmailValidator {
+    var email: String?
+    private var isValid: Bool = true
+    
+    func isValid(email: String) -> Bool {
+        self.email = email
+        return isValid
+    }
+    
+    func simulateInvalidEmail() {
+        isValid = false
+    }
+}
