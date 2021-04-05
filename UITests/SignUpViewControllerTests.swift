49429aadd5c01dd2649a21de91e30ff086cf09f3
Anderson Chagas <anderson.1997reis@gmail.com>
2021-04-05 19:54:33 -0300

    ensure SignUpController calls signUp when saveButton Tapped

--- UITests/SignUpViewControllerTests.swift
+++ UITests/SignUpViewControllerTests.swift
@@ -15,7 +15,6 @@ class SignUpViewControllerTests: XCTestCase {
 
     func test_loading_is_hidden_on_start() {
         let sut = makeSut()
-        sut.loadViewIfNeeded()
         XCTAssertEqual(sut.loadingIndicator?.isAnimating, false)
     }
 
@@ -29,12 +28,36 @@ class SignUpViewControllerTests: XCTestCase {
         XCTAssertNotNil(sut as AlertView)
     }
 
+    func test_saveButton_calls_signUp_on_tap() {
+        var callsCount = 0
+        let sut = makeSut(singUpSpy: { _ in callsCount += 1})
+        sut.saveButton?.simulateTap()
+        XCTAssertEqual(callsCount, 1)
+    }
+
 }
 
 extension SignUpViewControllerTests {
-    func makeSut() -> SignUpViewController {
+    func makeSut(singUpSpy: ((SignUpViewModel) -> Void)? = nil) -> SignUpViewController {
         let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
         let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
+        sut.signUp = singUpSpy
+        sut.loadViewIfNeeded()
         return sut
+    }
+}
+
+extension UIControl {
+
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            self.actions(forTarget: target, forControlEvent: event)?.forEach { action in
+                (target as NSObject).perform(Selector(action))
+            }
+        }
+    }
+
+    func simulateTap() {
+        simulate(event: .touchUpInside)
     }
 }
