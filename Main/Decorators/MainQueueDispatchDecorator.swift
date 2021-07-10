3c70dd7792559664065484705531435ebf813700
Anderson Chagas <anderson.1997reis@gmail.com>
2021-07-10 14:40:31 -0300

    move MainQueueDisptachDecorator to its own file

---
+++ Main/Decorators/MainQueueDispatchDecorator.swift
@@ -0,0 +1,34 @@
+//
+//  MainQueueDispatchDecorator.swift
+//  Main
+//
+//  Created by Anderson Chagas on 10/07/21.
+//  Copyright © 2021 Anderson Chagas. All rights reserved.
+//
+
+import Foundation
+import Domain
+
+public final class MainQueueDispatchDecorator<T> {
+
+    private let instance: T
+
+    public init(instance: T) {
+        self.instance = instance
+    }
+
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else { return DispatchQueue.main.async { completion() } }
+        completion()
+    }
+}
+
+extension MainQueueDispatchDecorator: AddAccount where T: AddAccount {
+
+    public func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, DomainError>) -> Void) {
+        instance.add(addAccountModel: addAccountModel) { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+
+}
