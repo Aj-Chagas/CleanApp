abcdec5420846daa5b13223bc0b2c951ae0e6510
Anderson Chagas <anderson.1997reis@gmail.com>
2021-08-02 13:07:56 -0300

    add authentication UseCase

---
+++ Domain/UseCase/Authentication.swift
@@ -0,0 +1,24 @@
+//
+//  Authentication.swift
+//  Domain
+//
+//  Created by Anderson Chagas on 02/08/21.
+//  Copyright © 2021 Anderson Chagas. All rights reserved.
+//
+
+import Foundation
+
+public protocol Authentication {
+    typealias Result = Swift.Result<AccountModel, DomainError>
+    func auth(addAccountModel: AuthenticationModel, completion: @escaping (Result) -> Void)
+}
+
+public struct AuthenticationModel: Model {
+    public var email: String
+    public var password: String
+
+    public init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
+}
